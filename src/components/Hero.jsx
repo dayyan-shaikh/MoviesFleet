a7fa4202c4817ab7b43664bfd0455c5047ab0d5e
Dayyan Shaikh <dayyanshaikh16@gmail.com>
2024-03-05 22:45:16 +0530

    commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,33 +2,32 @@ import React, { useEffect, useState } from "react";
 import Item from "./Item";
 
 const Hero = async () => {
-  const [record, setRecord] = useState([]);
-  let title="movie"
-  // const response = await fetch(`https://www.omdbapi.com/?apikey=87f0865a&s=${title}&page=1`);
-  
+  const [records, setRecords] = useState([]);
+
   useEffect(() => {
     fetch(`https://www.omdbapi.com/?apikey=87f0865a&s=don&page=1`)
       .then(response => response.json())
-      .then(res => setRecord(res))
+      .then(data => {
+        if (data.Search) {
+          setRecords(data.Search);
+        }
+      })
       .catch(err => console.log(err));
   }, []);
-  console.log(record);
+
   return (
     <div className="new-collections">
       <div className="collections">
-       
-      {/* {record.Search.map((item,i) => (
-              <Item key={i} Poster={item.Poster} Title={item.Title} Year={item.Year}/>
-              
-            ))} */}
+        {records.map((item, i) => (
+          <div key={i}>
+            <img src={item.Poster} alt={item.Title} />
+            <div>{item.Title}</div>
+            <div>{item.Year}</div>
+          </div>
+        ))}
       </div>
     </div>
   );
 };
 
-
-
-
-
 export default Hero;
-
