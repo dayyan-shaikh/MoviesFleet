eed27684154728de9da48673989ad0bdcfdd4454
Dayyan Shaikh <dayyanshaikh16@gmail.com>
2024-03-06 22:04:34 +0530

    commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,6 +6,9 @@ const Navbar = () => {
   const [searchTerm, setSearchTerm] = useState("");
   return (
     <div>
+      <div className="head">
+      <h1>MoviesFleet</h1>
+      </div>
        <form className="form" role="search">
         <input className="search" type="search" placeholder="Search"  value={searchTerm}
             onChange={(e) => setSearchTerm(e.target.value)}/>
