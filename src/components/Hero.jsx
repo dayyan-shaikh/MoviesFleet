22ce99d522f68d6413086fbe967aa4759a35c897
Dayyan Shaikh <dayyanshaikh16@gmail.com>
2024-03-08 17:24:31 +0530

    commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,11 +1,11 @@
 import React, { useEffect, useState } from "react";
-import './Hero.css'
+import "./Hero.css";
 
 const Hero = () => {
   const [records, setRecords] = useState([]);
   const [searchTerm, setSearchTerm] = useState("movie");
 
-  const searchMovie = ()=>{
+  const searchMovie = () => {
     fetch(`https://www.omdbapi.com/?apikey=87f0865a&s=${searchTerm}&page=1`)
       .then((response) => response.json())
       .then((data) => {
@@ -14,7 +14,7 @@ const Hero = () => {
         }
       })
       .catch((err) => console.log(err));
-  }
+  };
   useEffect(() => {
     fetch(`https://www.omdbapi.com/?apikey=87f0865a&s=${searchTerm}&page=1`)
       .then((response) => response.json())
@@ -24,41 +24,35 @@ const Hero = () => {
         }
       })
       .catch((err) => console.log(err));
- 
   }, []);
 
   return (
     <>
-    <form className="form" role="search">
+      <form className="form" role="search">
         <input
           className="search"
           type="search"
           placeholder="Search"
-          
           onChange={(e) => setSearchTerm(e.target.value)}
-        /> 
+        />
       </form>
-      {/* <button onClick={searchMovie}>Click me</button> */}
-      
+      <button className="btn btn-info" type="submit" onClick={searchMovie}>
+        Search
+      </button>
+
       <div className="container">
-    {records.map((item,i) => {
-        return (
-        
-            <div key={i} className="card"  style={{width: "18.7rem"}}>
+        {records.map((item, i) => {
+          return (
+            <div key={i} className="card" style={{ width: "18.7rem" }}>
               <img src={item.Poster} alt={item.Title} />
               <div className="card-body">
                 <h5 className="white">{item.Title}</h5>
-                <p className="white">
-                  {item.Year}
-                </p>
+                <p className="white">{item.Year}</p>
               </div>
             </div>
-          
-        );
-      })}
-      
-    </div>
-      
+          );
+        })}
+      </div>
     </>
   );
 };
