8950af0e24a0dec6bcd46cfb5779ec59e7f779f2
Dayyan Shaikh <dayyanshaikh16@gmail.com>
2024-03-10 17:36:32 +0530

    commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,10 @@
 import React from "react";
-import { useState } from "react";
 import "../components/Navbar.css";
 
 const Navbar = () => {
   return (
     <div>
-      <div className="head">
-        <h1>MoviesFleet</h1>
-      </div>
-      
+      <h1>MoviesFleet</h1>
     </div>
   );
 };
